d8c6d2dba210fa7b4d1b73b0d69de0a4a38a875f
Sam Hermes <samhermes@me.com>
2025-03-18 21:00:13 -0500

    Rearrange class

--- index.js
+++ index.js
@@ -7,25 +7,26 @@ class ScrollSnapControls {
     constructor(container) {
         this.activeIndex = 0;
         this.rowLength = 0;
+
+        this._setUpElements(container);
+        this._calculateRowLength();
+        this._setUpObserver();
+    }
+
+    _setUpElements(container) {
         this.galleryWrapper = container.querySelector(".gallery-wrapper");
         this.elements = [...this.galleryWrapper.querySelectorAll("li")];
 
-        // Establish initial row length value.
-        this.calculateRowLength();
-
         // Apply event listener to recalculate row length if the viewport width changes.
-        window.addEventListener('resize', this.calculateRowLength.bind(this));
-
-        // Set up observer.
-        this.setUpObserver();
+        window.addEventListener('resize', this._calculateRowLength.bind(this));
 
         // Apply event listeners to navigation buttons.
         container.querySelector('.gallery-nav-previous').addEventListener('click', this.scrollToPrevPage.bind(this));
         container.querySelector('.gallery-nav-next').addEventListener('click', this.scrollToNextPage.bind(this));
     }
 
-    setUpObserver() {
-        const observer = new IntersectionObserver(this.handleIntersect.bind(this), {
+    _setUpObserver() {
+        const observer = new IntersectionObserver(this._handleIntersect.bind(this), {
             root: galleryWrapper,
             rootMargin: "0px",
             threshold: 0.75
@@ -37,14 +38,14 @@ class ScrollSnapControls {
         });
     }
 
-    calculateRowLength() {
+    _calculateRowLength() {
         this.rowLength = Math.floor(
             this.galleryWrapper.clientWidth /
             this.elements[0].clientWidth
         );
     };
 
-    handleIntersect(entries) {
+    _handleIntersect(entries) {
         const entry = entries.find(e => e.isIntersecting);
         if (entry) {
             const index = this.elements.findIndex(
