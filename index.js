56493684d70705bf17973e5c6addab9c239432ec
Sam Hermes <samhermes@me.com>
2025-04-12 11:18:25 -0500

    Use functions instead, update default class names

--- index.js
+++ index.js
@@ -3,71 +3,128 @@
  *
  * @param {*} element The container element.
  */
-class ScrollSnapControls {
-    constructor(container) {
-        this.activeIndex = 0;
-        this.rowLength = 0;
 
-        this._setUpElements(container);
-        this._calculateRowLength();
-        this._setUpObserver();
+let settings = {
+    container: '.scroll-snap-container',
+    wrapper: '.scroll-snap-wrapper',
+    elementSelector: '.scroll-snap-container li',
+    previousSelector: '.scroll-snap-nav-previous',
+    nextSelector: '.scroll-snap-nav-next',
+    addControls: true,
+    paginated: true,
+}
+
+let elements = [];
+let intersectionObserver = null;
+let activeIndex = 0;
+
+const start = () => {
+    intersectionObserver = new IntersectionObserver(handleIntersect, {
+        root: document.querySelector(settings.selector),
+        rootMargin: "0px",
+        threshold: 0.75
+    });
+
+    elements = document.querySelectorAll(settings.elementSelector);
+
+    elements.forEach(el => {
+        intersectionObserver.observe(el);
+    });
+
+    if (settings.addControls) {
+        setUpButtons();
     }
 
-    _setUpElements(container) {
-        this.galleryWrapper = container.querySelector(".gallery-wrapper");
-        this.elements = [...this.galleryWrapper.querySelectorAll("li")];
+    attachNavHandlers();
+}
 
-        // Apply event listener to recalculate row length if the viewport width changes.
-        window.addEventListener('resize', this._calculateRowLength.bind(this));
+// Set up buttons, if enabled
+const setUpButtons = () => {
+    const container = document.querySelector(settings.container);
 
-        // Apply event listeners to navigation buttons.
-        container.querySelector('.gallery-nav-previous').addEventListener('click', this.scrollToPrevPage.bind(this));
-        container.querySelector('.gallery-nav-next').addEventListener('click', this.scrollToNextPage.bind(this));
-    }
+    const navigation = document.createElement('div');
+    navigation.classList.add('scroll-snap-nav');
 
-    _setUpObserver() {
-        const observer = new IntersectionObserver(this._handleIntersect.bind(this), {
-            root: galleryWrapper,
-            rootMargin: "0px",
-            threshold: 0.75
-        });
+    const previousButton = document.createElement('button');
+    previousButton.setAttribute('type', 'button');
+    previousButton.classList.add('scroll-snap-nav-previous');
+    previousButton.textContent = 'Previous';
 
-        // Add event listener to each element.
-        this.elements.forEach(el => {
-            observer.observe(el);
-        });
-    }
+    const nextButton = document.createElement('button');
+    nextButton.setAttribute('type', 'button');
+    nextButton.classList.add('scroll-snap-nav-next');
+    nextButton.textContent = "Next";
 
-    _calculateRowLength() {
-        this.rowLength = Math.floor(
-            this.galleryWrapper.clientWidth /
-            this.elements[0].clientWidth
+    navigation.appendChild(previousButton);
+    navigation.appendChild(nextButton);
+
+    container.appendChild(navigation);
+}
+
+const attachNavHandlers = () => {
+    document.querySelector(settings.previousSelector).addEventListener('click', scrollToPrevPage);
+    document.querySelector(settings.nextSelector).addEventListener('click', scrollToNextPage);
+}
+
+// Determine if user has reduced motion enabled.
+const hasReducedMotion = () => {
+    return window.matchMedia('(prefers-reduced-motion: reduce)').matches;
+}
+
+// Calculate taking in scroll offset and padding/gap
+// Can we do this using the initial count of intersection observer?
+const getRowLength = () => {
+    const container = document.querySelector(settings.wrapper);
+
+    return Math.floor(
+        container.clientWidth /
+        elements[0].clientWidth
+    );
+};
+
+const handleIntersect = (entries) => {
+    const entry = entries.find(e => e.isIntersecting);
+    const elementsArray = Array.from(elements);
+
+    if (entry) {
+        const index = elementsArray.findIndex(
+            e => e === entry.target
         );
-    };
-
-    _handleIntersect(entries) {
-        const entry = entries.find(e => e.isIntersecting);
-        if (entry) {
-            const index = this.elements.findIndex(
-                e => e === entry.target
-            );
-            this.activeIndex = index;
-        }
-    }
-
-    scrollToNextPage() {
-        if (this.activeIndex < this.elements.length - 1) {
-            this.elements[Math.min(this.activeIndex + (this.rowLength * 2), this.elements.length - 1)].scrollIntoView({
-                behavior: 'smooth'
-            })
-        }
-    }
-
-    scrollToPrevPage() {
-        if (this.activeIndex > 0) {
-            this.elements[Math.max(0, this.activeIndex - (this.rowLength * 2))].scrollIntoView({
-                behavior: 'smooth'
-            })
-        }
+        activeIndex = index;
     }
 }
+
+// Only do smooth scroll if reduced motion is on
+const scrollToNextPage = () => {
+    if (activeIndex < elements.length - 1) {
+        elements[Math.min(activeIndex + (getRowLength() * 2), elements.length - 1)].scrollIntoView({
+            behavior: hasReducedMotion() ? undefined : 'smooth',
+        })
+    }
+}
+
+// Only do smooth scroll if reduced motion is on
+const scrollToPrevPage = () => {
+    if (activeIndex > 0) {
+        elements[Math.max(0, activeIndex - (getRowLength() * 2))].scrollIntoView({
+            behavior: hasReducedMotion() ? undefined : 'smooth',
+        })
+    }
+}
+
+const updateSettings = (newSettings) => {
+    if (newSettings && newSettings !== settings) {
+        settings = {
+            ...settings,
+            ...newSettings,
+        };
+    }
+}
+
+const init = (settings) => {
+    updateSettings(settings);
+
+    start();
+}
+
+init();
